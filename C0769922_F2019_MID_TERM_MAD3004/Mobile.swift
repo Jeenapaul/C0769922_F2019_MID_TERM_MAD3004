b9db00e86ca3a3bf440184bc01e1d03f917eed89
Jeenapaul <jeenapaul1014@gmail.com>
2019-10-19 18:28:54 -0400

    6

--- C0769922_F2019_MID_TERM_MAD3004/Mobile.swift
+++ C0769922_F2019_MID_TERM_MAD3004/Mobile.swift
@@ -3,10 +3,19 @@
 import Foundation
 class Mobile : Bill
 {
-    var manufaName:String?
-    var planName:String?
-    var mobileNumber:String?
-    var gbUsed:Float?
-    var minutesUsed:Int?
-    var internetBill : Float?
+    var manufacturerName:String
+    var planName:String
+    var mobileNumber:String
+    var gbUsed:Float
+    var minutesUsed:Int
+    var billAmount : Float
+    init(manufacturerName:String,planName:String,mobileNumber:String,gbUsed:Float,minutesUsed:Int,billAmount:Float,billId : String,billType : BillingTypes,billDate: String) {
+        self.manufacturerName=manufacturerName
+        self.planName=planName
+        self.mobileNumber=mobileNumber
+        self.gbUsed=gbUsed
+        self.minutesUsed=minutesUsed
+        self.billAmount=billAmount
+        super.init(billID: billID, billDate: billDate, billType: billType)
+    }
 }

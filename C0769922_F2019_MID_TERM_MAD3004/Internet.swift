4ebe899202b6a977f88bf55e98aa433e77f4ea1a
Jeenapaul <jeenapaul1014@gmail.com>
2019-10-19 18:37:42 -0400

    10

--- C0769922_F2019_MID_TERM_MAD3004/Internet.swift
+++ C0769922_F2019_MID_TERM_MAD3004/Internet.swift
@@ -5,5 +5,15 @@ class Internet : Bill{
     var providerName : String
     var internetUsed : Float
     var billAmount : Float
+    init(providerName : String, gbUsed : Float, billId : String, billDate : String,billAmount: Float, billType : Bill.BillingTypes) {
+        self.providerName = providerName
+        self.internetUsed = gbUsed
+        self.billAmount = billAmount
+        super.init(billID: billId, billDate: billDate, billType: billType)
+        
+    }
+        
+        
+    
     
 }

bea42a7f9273f1c0869ab68754de8c770ce09886
Jeenapaul <jeenapaul1014@gmail.com>
2019-10-19 18:57:36 -0400

    16

--- C0769922_F2019_MID_TERM_MAD3004/Customer.swift
+++ C0769922_F2019_MID_TERM_MAD3004/Customer.swift
@@ -11,3 +11,13 @@ class Customer : IDisplay
     var fullName : String?
     var email : String?
  var bDictionary = Dictionary<String,Bill>()
+    init(customerId : String, firstName : String, lastName : String, email : String ,bDictionary : Dictionary<String,Bill>)
+    {
+        self.customerId = customerId
+        self.firstName = firstName
+        self.lastName = lastName
+        self.email = email
+        self.bDictionary = bDictionary
+        self.fullName = firstName + lastName
+}
+}

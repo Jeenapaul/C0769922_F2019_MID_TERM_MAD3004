7a3de2c512384e8c524f67da7a03ba74888088d9
Jeenapaul <jeenapaul1014@gmail.com>
2019-10-19 18:38:58 -0400

    11

--- C0769922_F2019_MID_TERM_MAD3004/Internet.swift
+++ C0769922_F2019_MID_TERM_MAD3004/Internet.swift
@@ -5,9 +5,9 @@ class Internet : Bill{
     var providerName : String
     var internetUsed : Float
     var billAmount : Float
-    init(providerName : String, gbUsed : Float, billId : String, billDate : String,billAmount: Float, billType : Bill.BillingTypes) {
+    init(providerName : String, internetUsed : Float, billId : String, billDate : String,billAmount: Float, billType : Bill.BillingTypes) {
         self.providerName = providerName
-        self.internetUsed = gbUsed
+        self.internetUsed = internetUsed
         self.billAmount = billAmount
         super.init(billID: billId, billDate: billDate, billType: billType)
         

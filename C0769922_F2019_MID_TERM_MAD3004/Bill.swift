2b90b881104803d7b8774a2ec70459e2e18fc957
Jeenapaul <jeenapaul1014@gmail.com>
2019-10-19 18:01:50 -0400

    3

--- C0769922_F2019_MID_TERM_MAD3004/Bill.swift
+++ C0769922_F2019_MID_TERM_MAD3004/Bill.swift
@@ -1,12 +1,18 @@
 
 
 import Foundation
-class Bill : IDisplay
-{
-    func iDisplay() {
+class Bill IDisplay{
+
+    func IDisplay() {
         
     }
-
+    var billId : String?
+    var billDate :  String
+    enum BillingTypes {
+        case Internet
+        case Hydro
+        case Mobile
+    }
 }
 
 

9239b2b32ff30571deb7dc21fd3b7175fa2d9afd
Jeenapaul <jeenapaul1014@gmail.com>
2019-10-19 19:18:55 -0400

    18

--- C0769922_F2019_MID_TERM_MAD3004/main.swift
+++ C0769922_F2019_MID_TERM_MAD3004/main.swift
@@ -2,8 +2,9 @@
 import Foundation
 
 var date = Date()
-//var result = date.dateformatter()
+
 var cDictionary = Dictionary<String,Customer>()
 var totalBill : Float
-var i1 = Internet(providerName: "Chaters", internetUsed : 35.8, billID : "001", billDate : "Jan 25", billAmount : 38.6, billType : Bill.BillingTypes.Internet)
+var i1 = Internet(providerName: "Chaters", internetUsed : 35.8, billID : "i101", billDate : "Jan 25", billAmount : 38.6, billType : Bill.BillingTypes.Internet)
 
+var m1 = Mobile(manufacturerName: "Apple", planName : "Full Package", mobileNumber : "59178785", gbUsed : 18.6, minutesUsed : 673, billAmount : 104, billId : "m101", billType :Bill.BillingTypes.Mobile, billDate: "4 mar 2019" )
